50826341a0f7ee76d52c4e911d1ddc80eba7fe0f
Marco Perez Alvarez <mperez.alvarez97@gmail.com.com>
2023-01-28 21:46:35 -0500

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './repos/database/database.module';
+import { ReposModule } from './repos/repos.module';
+import {ConfigModule} from '@nestjs/config';
+import config from './config';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+   // envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+    load: [config],
+    isGlobal: true,
+  }),ReposModule,DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
